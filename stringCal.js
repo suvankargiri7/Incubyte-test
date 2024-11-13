6783037d8fd68978d475d4290ad97501fdffbbbb
suvankargiri7 <suvankargiri7@gmail.com>
2024-11-13 18:49:46 +0530

    refactor code from function to class for add method

--- stringCal.js
+++ stringCal.js
@@ -1,17 +1,21 @@
-const stringCal = (input) => {
-  if (input === "") return 0;
-  let delimiter = ",";
-  if (input.startsWith("//")) {
-    const inputParts = input.split("\n");
-    delimiter = inputParts[0][2];
-    input = inputParts[1];
+class StringCal {
+  add(input) {
+    if (input === "") return 0;
+    let delimiter = ",";
+    if (input.startsWith("//")) {
+      const inputParts = input.split("\n");
+      delimiter = inputParts[0][2];
+      input = inputParts[1];
+    }
+    const numbers = input
+      .split(new RegExp(`[${delimiter}\n,]`))
+      .map((num) => parseInt(num));
+    const negatives = numbers.filter((number) => number < 0);
+    if (negatives.length > 0) {
+      throw new Error(`negative numbers not allowed ${negatives.join(",")}`);
+    }
+    return numbers.reduce((sum, number) => sum + number, 0);
   }
-  const numbers = input.split(new RegExp(`[${delimiter}\n,]`)).map(num => parseInt(num));
-  const negatives = numbers.filter(number => number < 0);
-  if (negatives.length > 0) {
-    throw new Error(`negative numbers not allowed ${negatives.join(",")}`);
-  }
-  return numbers.reduce((sum, number) => sum + number, 0);
-};
+}
 
-module.exports = stringCal;
+module.exports = StringCal;
--- stringCal.test.js
+++ stringCal.test.js
@@ -1,32 +1,33 @@
-const stringCal = require("./stringCal");
+const StringCal = require("./stringCal");
+
+const stringCal = new StringCal();
 
 test("should return 0 for an empty string", () => {
-  expect(stringCal("")).toBe(0);
+  expect(stringCal.add("")).toBe(0);
 });
 
-test('should return the number if only one number is provided', () => {
-  expect(stringCal("10")).toBe(10);
+test("should return the number if only one number is provided", () => {
+  expect(stringCal.add("10")).toBe(10);
 });
 
-test('should return the sum of two numbers separated by a comma', () => {
-  expect(stringCal("1,5")).toBe(6);
+test("should return the sum of two numbers separated by a comma", () => {
+  expect(stringCal.add("1,5")).toBe(6);
 });
 
-test('should return the sum of any multiple numbers separated by commas', () => {
-  expect(stringCal("1,5,5")).toBe(11);
+test("should return the sum of any multiple numbers separated by commas", () => {
+  expect(stringCal.add("1,5,5")).toBe(11);
 });
 
-test('should allow new lines between numbers', () => {
-  expect(stringCal("1\n2,3")).toBe(6);
+test("should allow new lines between numbers", () => {
+  expect(stringCal.add("1\n2,3")).toBe(6);
 });
 
-test('should support different delimiters', () => {
-  expect(stringCal("//;\n1;2")).toBe(3);
+test("should support different delimiters", () => {
+  expect(stringCal.add("//;\n1;2")).toBe(3);
 });
 
-test('should throw an error if negative numbers are there', () => {
-  expect(() => stringCal("5,-6,7,-8")).toThrow("negative numbers not allowed -6,-8");
+test("should throw an error if negative numbers are there", () => {
+  expect(() => stringCal.add("5,-6,7,-8")).toThrow(
+    "negative numbers not allowed -6,-8"
+  );
 });
-
-
-
