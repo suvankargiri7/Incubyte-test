8e034bb16b08019daba53bdcc22e2059c58289ea
suvankargiri7 <suvankargiri7@gmail.com>
2024-11-13 18:34:47 +0530

    added implementation of negative numbers not allowed

--- stringCal.js
+++ stringCal.js
@@ -7,6 +7,10 @@ const stringCal = (input) => {
     input = inputParts[1];
   }
   const numbers = input.split(new RegExp(`[${delimiter}\n,]`)).map(num => parseInt(num));
+  const negatives = numbers.filter(number => number < 0);
+  if (negatives.length > 0) {
+    throw new Error(`negative numbers not allowed ${negatives.join(",")}`);
+  }
   return numbers.reduce((sum, number) => sum + number, 0);
 };
 
