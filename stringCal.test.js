b333549edfe6123c97bc818972b7937e6bdc89ad
suvankargiri7 <suvankargiri7@gmail.com>
2024-11-13 17:47:01 +0530

    test case of sum of comma separated values in a string

--- stringCal.test.js
+++ stringCal.test.js
@@ -7,3 +7,8 @@ test("should return 0 for an empty string", () => {
 test('should return the number if only one number is provided', () => {
   expect(stringCal("10")).toBe(10);
 });
+
+test('should return the sum of two numbers separated by a comma', () => {
+  expect(stringCal("1,5")).toBe(6);
+});
+
