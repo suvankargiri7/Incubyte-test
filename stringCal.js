335c4492b2be028c5276e2a4ff6fbc04c507b2fb
suvankargiri7 <suvankargiri7@gmail.com>
2024-11-13 18:25:44 +0530

    implementation of support custom delimiters

--- stringCal.js
+++ stringCal.js
@@ -1,6 +1,12 @@
 const stringCal = (input) => {
   if (input === "") return 0;
-  const numbers = input.split(new RegExp("[\n,]")).map(num => parseInt(num));
+  let delimiter = ",";
+  if (input.startsWith("//")) {
+    const inputParts = input.split("\n");
+    delimiter = inputParts[0][2];
+    input = inputParts[1];
+  }
+  const numbers = input.split(new RegExp(`[${delimiter}\n,]`)).map(num => parseInt(num));
   return numbers.reduce((sum, number) => sum + number, 0);
 };
 
